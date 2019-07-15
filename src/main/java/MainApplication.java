f5d8a7e1a6f76ff9e3caa44f6cd4329a954adb44
Michał Doliński <micdolinski@gmail.com>
2019-07-15 22:10:34 +0200

    init

---
+++ src/main/java/MainApplication.java
@@ -0,0 +1,33 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class MainApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL url = getClass().getClassLoader().getResource("fxml/main_window.fxml");
+        FXMLLoader loader = new FXMLLoader(url);
+
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("REJ006");
+        primaryStage.setMinWidth(300);
+        primaryStage.setMinHeight(200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+    }
+}
